f652d4ab2f0364e07c1d0549e9d6a3dd3f2b8809
Terricola <visoc4@gmail.com>
2022-10-27 12:10:06 -0500

    there's no change here

--- challenges/first_MVT/family_data/models.py
+++ challenges/first_MVT/family_data/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class BasicData(models.Model):
+
     name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
