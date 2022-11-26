c7ccced55413cb8e44c6df4b6b763e1bed3bfe4a
Terricola <visoc4@gmail.com>
2022-11-25 19:07:35 -0500

    Estado del proyecto antes de la eliminacion de Heroku

--- final_project/yajuu/home/forms.py
+++ final_project/yajuu/home/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
+from home.models import Avatar
+
 class UserRegisterForm(UserCreationForm):
 
     username = forms.CharField(label="Usuario", min_length=3)
@@ -21,3 +23,22 @@ class UserRegisterForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+class UserUpdateForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+        ]
+        widgets = {
+            "email": forms.TextInput(attrs={"readonly": "readonly"}),
+        }
+
+class AvatarForm(forms.ModelForm):
+    class Meta:
+        model = Avatar
+        fields = ("image", )
--- final_project/yajuu/yajuu/settings.py
+++ final_project/yajuu/yajuu/settings.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_heroku
+#import django_heroku
 import dj_database_url
-import environ
+#import environ
 import os
 from django.contrib.messages import constants as messages
 from pathlib import Path
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-+_3un2mm9y00*t#vd09*gu*v&x2te)=xv4=-4w4_v-j!7m2r!6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -151,14 +151,14 @@ MESSAGE_TAGS = {
 
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL = '/post-detail'
+LOGIN_REDIRECT_URL = '/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Heroku configs
-django_heroku.settings(locals())
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# django_heroku.settings(locals())
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
